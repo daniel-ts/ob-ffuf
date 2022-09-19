c669aba7a014d2ae074ebe0ceb134967777707d4
Daniel Tschertkow <daniel.tschertkow@posteo.de>
2022-09-19 11:16:06 +0200

    Fixes package-lint warnings

--- ob-ffuf-mode.el
+++ ob-ffuf-mode.el
@@ -1,8 +1,10 @@
 ;;; ob-ffuf-mode.el --- syntax highlight for ob-ffuf
 
 ;; Copyright (C) 2022 Daniel Tschertkow
+;; Version: 0.9
+;; URL: https://github.com/daniel-ts/ob-ffuf
 
-;;; License
+;;; License:
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -17,15 +19,18 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary
+;;; Commentary:
 
-;; This code to highlight HTTP syntax is taken from ob-http
+;; This code highlights HTTP syntax and is taken from ob-http
 ;; (<https://github.com/zweifisch/ob-http>) by Feng Zhou.
 
+;;; Code:
+(defvar ob-ffuf--mode-keywords nil "Mode keywords for ob-ffuf.")
+
 (setq ob-ffuf--mode-keywords
-      (let* ((ffuf-methods
+      (let* ((http-methods
               '(GET POST PUT PATCH DELETE OPTIONS HEAD TRACE CONNECT))
-             (ffuf-headers
+             (http-headers
               '(Accept Accept-Charset Accept-Encoding Accept-Language
                        Accept-Datetime Authorization Cache-Control
                        Connection Cookie Content-Length Content-MD5
@@ -36,38 +41,40 @@
 		       Sec-Fetch-Mode Sec-Fetch-Site TE
 		       Upgrade-Insecure-Requests User-Agent Upgrade Via
 		       Warning))
-             (ffuf-methods-regexp
+             (http-methods-regexp
               (rx-to-string
                `(seq
                  bol
                  (? (1+ space))
-                 (group-n 1 (or ,@(mapcar 'symbol-name ffuf-methods)))
+                 (group-n 1 (or ,@(mapcar 'symbol-name http-methods)))
                  space
                  (group-n 2 (1+ any))
                  eol)))
-             (ffuf-headers-regexp
+             (http-headers-regexp
               (rx-to-string
                `(seq
                  bol
                  (? (1+ space))
-                 (group-n 1 (or ,@(mapcar 'symbol-name ffuf-headers)))
+                 (group-n 1 (or ,@(mapcar 'symbol-name http-headers)))
                  ": "
                  (group-n 2 (1+ any))
                  eol)))
-             (ffuf-custom-headers-regexp
+             (http-custom-headers-regexp
               "\\(^X-[^ :]+\\): \\(.*\\)$")
-             (ffuf-variable-regexp
+             (http-variable-regexp
               "\\([^ ?&=\n]+\\)=\\([^&\n]*\\)")
-             (ffuf-misc-regexp
+             (http-misc-regexp
               "\\(&\\|=\\|?\\|{\\|}\\|\\[\\|\\]\\|\\,\\|:\\)"))
-        `((,ffuf-headers-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
-          (,ffuf-custom-headers-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
-          (,ffuf-variable-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
-          (,ffuf-methods-regexp  (1 font-lock-constant-face) (2 font-lock-function-name-face))
-          (,ffuf-misc-regexp (1 font-lock-comment-face)))))
+        `((,http-headers-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
+          (,http-custom-headers-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
+          (,http-variable-regexp (1 font-lock-variable-name-face) (2 font-lock-string-face))
+          (,http-methods-regexp  (1 font-lock-constant-face) (2 font-lock-function-name-face))
+          (,http-misc-regexp (1 font-lock-comment-face)))))
 
-(define-derived-mode ffuf-mode fundamental-mode "ffuf"
+(define-derived-mode ob-ffuf-mode fundamental-mode "ffuf"
   (set (make-local-variable 'font-lock-defaults) '(ob-ffuf--mode-keywords)))
+
+(add-to-list 'org-src-lang-modes '("ffuf" . ob-ffuf))
 
 (provide 'ob-ffuf-mode)
 ;;; ob-ffuf-mode.el ends here
